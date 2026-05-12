945bce0ccac1eb7654a6463ca17f3eb1292f992d
Solomon Adeniran <soluwaseun806@gmail.com>
2026-05-12 16:08:34 +0100

    Add login/signup templates and styles

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/services/auth';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
-export class Login {
+export class Login implements OnInit{
+  loginForm!:FormGroup;
+  constructor(
+    private fb:FormBuilder,
+    private authService : AuthService,
+    private router : Router
+  ){}
+
+  ngOnInit(){ 
+    this.loginForm = this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required]]
+  })
+}
+
+  login(){
+    if(this.loginForm.invalid){
+      return
+    }
+
+    this.authService
+      .login(this.loginForm.value)
+      .subscribe({
+        next:(res:any) => {
+          this.authService.saveToken(res.access_token)
+
+          const user = this.authService.getCurrentUser();
+
+          if (user.role === 'admin') {
+            this.router.navigate(['/admin']);
+          } else {
+            this.router.navigate(['/home']);
+          }
+
+        },
+
+        error: (err) => {
+          console.log(err.error.message);
+        }
+      })
+  }
 
 }
--- frontend/src/app/features/auth/signup/signup.ts
+++ frontend/src/app/features/auth/signup/signup.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import { AuthService } from '../../../core/services/auth';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './signup.html',
-  styleUrl: './signup.scss',
+  styleUrls: ['./signup.scss'],
 })
-export class Signup {
+export class Signup implements OnInit{
+  signupForm!: FormGroup
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+states = [
+  'Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi',
+  'Bayelsa', 'Benue', 'Borno', 'Cross River', 'Delta',
+  'Ebonyi', 'Edo', 'Ekiti', 'Enugu', 'FCT', 'Gombe',
+  'Imo', 'Jigawa', 'Kaduna', 'Kano', 'Katsina', 'Kebbi',
+  'Kogi', 'Kwara', 'Lagos', 'Nasarawa', 'Niger', 'Ogun',
+  'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto',
+  'Taraba', 'Yobe', 'Zamfara'
+]
+
+  ngOnInit(){
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      state: ['', Validators.required]
+    });
+  }
+
+  signup() {
+
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    this.authService
+      .signup(this.signupForm.value)
+      .subscribe({
+
+        next: () => {
+
+          console.log('Signup successful');
+
+          this.router.navigate(['/login']);
+        },
+
+        error: (err) => {
+          console.log(err.error.message);
+        }
+
+      });
+  }
 }
